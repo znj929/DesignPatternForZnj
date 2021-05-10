f0775441f4353a219d14a3b2576438738d3ec558
119503 <119503>
2021-05-10 10:33:01 +0800

    @Override

--- design/src/main/java/template/AbstractDiscountCalculatorl.java
+++ design/src/main/java/template/AbstractDiscountCalculatorl.java
@@ -5,6 +5,7 @@ package template;
  * @Date: 2021/5/7 0007 21:39
  */
 public abstract class AbstractDiscountCalculatorl implements DiscountCalculator{
+    @Override
     public void calculate(){
         System.out.println("先使用 通用优惠。");
         spec();
